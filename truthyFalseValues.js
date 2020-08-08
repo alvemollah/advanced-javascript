0ae1e45985a12b92f60cdefa28f6f14bf514728a
alvemollah <alvealex71@gmail.com>
2020-08-08 18:20:51 +0600

    truthy vs falsy

---
+++ truthyFalseValues.js
@@ -0,0 +1,218 @@
+//Here I am gonn defined falsy values
+
+//1
+
+let number = 0;
+
+if(number){
+    console.log("this number is true");
+}
+
+else{
+    console.log('this number is false');
+}
+
+//2
+
+ let string = "";
+
+ if(string){
+    console.log("this number is true");
+}
+
+else{
+    console.log('this number is false');
+}
+
+//3
+
+let fal1 = undefined;
+
+if(fal1){
+    console.log("this number is true");
+}
+
+else{
+    console.log('this number is false');
+}
+
+
+//4
+
+let fal2 = null;
+
+if(fal2){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}
+
+//5
+
+let fal3 = undefined;
+
+if(fal3){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}
+
+//6
+
+let fal4 = false;
+
+if(fal4){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}
+
+//7
+
+let fal5 = 0n;
+
+if(fal5){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}
+
+
+// Here I am  gonna defined truthy values
+
+//1
+
+const tru1 = true;
+
+if(tru1){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}
+
+//2
+
+
+const tru2 = {};
+
+if(tru2){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}   
+
+//3
+
+
+const tru3 = [];
+
+if(tru3){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}
+
+//4
+
+
+const tru4 = 34;
+
+if(tru4){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}
+
+//5
+
+
+const tru5 = "0";
+
+if(tru5){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}
+
+//6
+
+
+const tru6 = -34;
+
+if(tru6){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}
+
+//7
+
+
+const tru7 = Infinity;
+
+if(tru7){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}
+
+//8
+
+
+const tru8 = -Infinity;
+
+if(tru8){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}
+
+//9
+
+
+const tru9 = -Infinity;
+
+if(tru9){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}
+
+//10
+
+
+const tru10 = "false";
+
+if(tru10){
+    console.log("this is a true");
+}
+
+else{
+    console.log("this is a false")
+}
